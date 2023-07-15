04aee8630a5fb9ffd5b4a0b09ddfc30c045a538a
giovanidli <giovanilima12390@gmail.com>
2023-07-15 11:00:58 -0300

    Initial

---
+++ src/utils/load-posts.js
@@ -0,0 +1,17 @@
+export const loadPosts = async () => {
+
+    const postResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    // map retorna novo array
+    const postsAndPhotos = postsJson.map((post, index) => {
+        return { ...post, cover: photosJson[index].url }
+    });
+
+    return postsAndPhotos;
+}
